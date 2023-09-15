4c6a070da574309f7bb67ab9750111251c723db6
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-09-16 00:54:03 +0900

    :sparkles: genderのenum追加と対応するviewとcontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @gender_options = User.genders_i18n.invert
   end
 
   def create
@@ -11,12 +12,13 @@ class UsersController < ApplicationController
       auto_login(@user)
       redirect_to events_path, success: 'ユーザー登録が完了しました'
     else
+      @gender_options = User.genders_i18n.invert
       flash.now[:danger] = 'ユーザー登録に失敗しました'
       render :new
     end
   end
 
   def user_params
-    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    params.require(:user).permit(:email, :name, :gender, :password, :password_confirmation)
   end
 end
