e70049cbc84e49a5193e338974c5152c257d0c5f
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-09-18 16:44:14 +0900

    :+1: not null制約の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_15_160844) do
+ActiveRecord::Schema.define(version: 2023_09_18_071438) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -136,7 +136,7 @@ ActiveRecord::Schema.define(version: 2023_09_15_160844) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "gender", default: 0
+    t.integer "gender", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
